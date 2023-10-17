b887294f32ebe000542630000820f2ab5f0b6907
srush23 <74239252+SrushtiHaryan@users.noreply.github.com>
2023-10-17 16:36:45 +0530

    sol2

--- Solution2.java
+++ Solution2.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Solution2 {
+
+     public static int puzzle(int h, int u, int s){
+        int differ = h - s;
+        int hours = (h-u)/differ;
+        return hours+1;
+    }
+        public static void main(String[] args) {  
+            Scanner sc = new Scanner(System.in);
+            int H = sc.nextInt();
+            int U = sc.nextInt();
+            int S = sc.nextInt();
+            int ans = puzzle(H, U, S); 
+            System.out.println(ans);
+            sc.close();
+        } 
     
 }
